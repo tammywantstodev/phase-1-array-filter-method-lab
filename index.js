4b433c6eb4ed56b961215ddd666273937a2004f0
Tamara Mwanzali Kaka <billnyetherussianspy679@gmail.com>
2024-11-27 17:14:17 +0300

    Saving changes

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(arr,str){
+    return arr.filter((driver)=>{
+      return driver.toLowerCase()===str.toLowerCase();});
+  
+  }
+  function fuzzyMatch(arr,str){
+    return arr.filter((driver)=>{
+      return driver.startsWith(str);
+    })
+  }
+  function matchName(arr,str){
+    return arr.filter((driver)=>{
+     return driver.name=== str
+    })
+  }
